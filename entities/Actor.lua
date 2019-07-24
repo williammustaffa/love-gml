b08d99fa1e114391979bdb5fc24963857ddf5cbd
williammustaffa <william.lima.cat@gmail.com>
2019-07-24 17:11:38 -0300

    base

---
+++ entities/Actor.lua
@@ -0,0 +1,18 @@
+-- File: Actor.lua
+-- TODO: documentate class
+
+Actor = Object:extend()
+
+function Actor:new()
+  -- Actor creation 
+end
+
+function Actor:update(dt)
+  -- Actor update
+end
+
+function Actor:draw()
+  -- Actor drawing
+end
+
+return Actor
---
+++ main.lua
@@ -0,0 +1,14 @@
+function love.load()
+  -- Load libs
+  Object = require "libs.classic"
+  Tick = require "libs.tick"
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
